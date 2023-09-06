f067f5dcb94dba0ba26b8c6200ddc49a899c98a9
gabriella jonsson <gabriella.jonsson@elev.ga.ntig.se>
2023-09-06 10:59:49 +0200

    lagt till kod för heltals operationer

--- src/App.java
+++ src/App.java
@@ -5,8 +5,20 @@ public class App {
     {
         System.out.println("Hello, World!");
 
-        int tal = 30;
-        
+        int tal1 = 2;
+        int tal2 = 30;
+
+        int summa_addition = tal1+tal2;
+        int summa_subtraktion  = tal1-tal2;
+
+        System.out.println("summa an tal + tal2 är " + summa_addition);
+        System.out.println("summa av tal    tal2 är " +summa_subtraktion);
+
+        int produkt = tal1*tal2;
+        int kvot = tal1/tal2
+
+        System.out.println("produkten av tal1*tal2 är " + produkt);
+        System.out.println("kvoten av tal1/tal2 är " +kvot)
 
     }
 }
